1cfa41ed6c0fa774690999a17185b4a112727f01
andrewhlu <alu@ucsb.edu>
2019-04-21 18:29:25 -0700

    Remove some old code

--- server.py
+++ server.py
@@ -1,27 +1,13 @@
 import json
 from websocket_server import WebsocketServer
-import board
-import neopixel
-
-# Initialize pixels
-pixelPin = board.D18
-numPixels = 198
- 
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-# ORDER = neopixel.RGB
- 
-pixels = neopixel.NeoPixel(pixelPin, numPixels)
-
-# Syntax to change LED
-# pixels[LED_NUMBER] = (RED_VALUE, GREEN_VALUE, BLUE_VALUE)
-# RGB values between 0 and 255
-
+import serial
+import time
+import math
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
-	print("New client connected and was given led %d" % client["id"])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	print("New client connected and was given ID %d" % client["id"])
+	server.send_message_to_all(json.dumps(board))
 
 # Called for every client disconnecting
 def client_left(client, server):
@@ -31,13 +17,39 @@ def client_left(client, server):
 def message_received(client, server, message):
 	print(message)
 	data = json.loads(message)
-	print(data["led"])
-	pixels[data["led"]] = (data["red"], data["blue"], data["green"])
-	pixels.show()
-	print("Turning on LED #%d" % data["led"])
+
+	if data["led"] == -1:
+		clear_board()
+	else:
+		board[data["led"]] = data["color"]
+		send_led(data["led"], data["color"])
+		server.send_message_to_all(json.dumps(board))
+
+def send_led(led, color):
+	# Send Color
+	ser.write(color.encode('ascii'))
+
+	# Send LED number in two bytes
+	ser.write(str(chr(math.floor(led / 2))).encode('ascii'))
+	ser.write(str(chr(math.ceil(led / 2))).encode('ascii'))
+
+def clear_board():
+	for x in range(0,198):
+		board[x] = "o"
+		send_led(x, "o")
+
+	server.send_message_to_all(json.dumps(board))
+
+# Startup Code
+ser = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(2)
+
+board = {}
+for x in range(0,198):
+	board[x] = "o"
 
 PORT=9001
-server = WebsocketServer(PORT)
+server = WebsocketServer(PORT, "0.0.0.0")
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
